921ed70c88dc50911a987b16d6187bf4ca55ba8c
Victor Le <victor.le@epitech.eu>
2018-03-16 10:07:26 +0100

    CLISystem use CmdManager

--- S/CLISystem/CLISystem.cpp
+++ S/CLISystem/CLISystem.cpp
@@ -1,25 +1,57 @@
 #include "CLISystem.hh"
 #include "CoreEvent.hh"
 #include "EManager.hh"
-#include "parsing/CLIParserOutputs.hh"
+#include <experimental/source_location>
 
 CLISystem::CLISystem()
-  : cliproducer{}
-  , cliparser{cliproducer}
+  : _cliproducer{}
+  , _cliparser{_cliproducer}
+  , _cmds{
+    {
+      "quit",
+      [](const lel::CmdOutput&) -> void
+      {
+        EManager::fire<CoreEvent>(CoreEvent::Type::EXIT);
+      }
+    },
+    {
+      "add",
+      [](const lel::CmdOutput& o) -> void
+      {
+        if (o.getArgs().size() != 1)
+        {
+          std::cout << "Usage: add <system name>\n";
+          return ;
+        }
+
+        auto sysName = o.getArgs()[0]->getTerminal();
+        EManager::fire<CoreEvent>(CoreEvent::Type::ADD_SYSTEM, sysName);
+      }
+    },
+  } //CLISystem::_cmds end of initialization
 {}
 
 void CLISystem::exec()
 {
-  auto expr = cliparser.parseExpression();
-  if (expr->getType() != lel::CLIParserType::COMMAND)
+  try
   {
-    std::cout << "This is not a command\n";
+    auto expr = _cliparser.parseExpression();
+
+    if (expr->getType() != lel::CLIParserType::COMMAND)
+    {
+      std::cout << "This is not a command\n";
+      return ;
+    }
+
+    auto cmd = std::static_pointer_cast<lel::CmdOutput>(expr);
+    _cmds.exec(*cmd);
+    _cliparser.consume(lel::CLIProducerType::EOL);
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << std::experimental::source_location::current().function_name()
+      << ": " << e.what() << "\n";
+    _cliparser.reset();
     return ;
   }
-  auto cmd = std::static_pointer_cast<lel::CmdOutput>(expr);
-  if (cmd->getCommand() == "quit")
-    EManager::fire<CoreEvent>(CoreEvent::Type::EXIT);
-  else
-    std::cout << "Unknown command '" << cmd->getCommand() << "'\n";
-  cliparser.consume(lel::CLIProducerType::EOL);
 }
--- S/CLISystem/CLISystem.hh
+++ S/CLISystem/CLISystem.hh
@@ -2,6 +2,32 @@
 
 #include "CRTPS.hpp"
 #include "parsing/CLIParser.hh"
+#include "parsing/CLIParserOutputs.hh"
+#include <functional>
+
+class CmdManager
+{
+public:
+  using Command = std::string;
+  using Callback = std::function<void(const lel::CmdOutput&)>;
+
+public:
+  CmdManager(std::initializer_list<std::unordered_map<Command, Callback>::value_type> l)
+    : _actions{l}
+  {}
+
+  void exec(const lel::CmdOutput& output)
+  {
+    auto it = _actions.find(output.getCommand());
+    if (it == std::end(_actions))
+      std::cout << "Unknown command '" << output.getCommand() << "'\n";
+    else
+      it->second(output);
+  }
+
+private:
+  std::unordered_map<Command, Callback> _actions;
+};
 
 class CLISystem : public CRTPS<CLISystem>
 {
@@ -11,6 +37,7 @@ public:
   void exec() override;
 
 private:
-  lel::CLIProducer cliproducer;
-  lel::CLIParser cliparser;
+  lel::CLIProducer _cliproducer;
+  lel::CLIParser _cliparser;
+  CmdManager _cmds;
 };
