f4046ef612293a8beb08fb3a6427181e74e841c7
Victor Le <victor.le@epitech.eu>
2018-03-18 13:46:56 +0100

    Update EManagerEvent

--- E/EManagerEvent/EManagerEvent.cpp
+++ E/EManagerEvent/EManagerEvent.cpp
@@ -4,12 +4,12 @@ EManagerEvent::EManagerEvent(const Type t)
   : _t{t}
 {}
 
-EManagerEvent::EManagerEvent(const Type t, ID id)
-  : _t{t}
-  , _idData{id}
-{}
-
 EManagerEvent::Type EManagerEvent::getType() const
 {
   return _t;
+}
+
+EManagerEvent::Data EManagerEvent::getData() const
+{
+  return _data;
 }
--- E/EManagerEvent/EManagerEvent.hh
+++ E/EManagerEvent/EManagerEvent.hh
@@ -2,10 +2,13 @@
 
 #include "CRTPE.hpp"
 #include "emanagerevent_export.h"
+#include <variant>
+#include <string>
 
 class EMANAGEREVENT_EXPORT EManagerEvent : public CRTPE<EManagerEvent>
 {
-  using ID = unsigned; //SIDGenerate::ID
+public:
+  using Data = std::variant<ID, std::string>;
 
 public:
   enum class Type : char
@@ -18,17 +21,24 @@ public:
 
     // ERROR
     NOT_LISTENER,
+    LISTENER_NOT_FOUND,
+    LISTENER_ALREADY_ADDED,
+    EVENT_ALREADY_ADDED,
   };
 
 public:
   EManagerEvent(const Type t = Type::UNKNOWN);
-  EManagerEvent(const Type t, ID id);
+  template <typename T>
+  EManagerEvent(const Type t, T&& data)
+    : _t{t}
+    , _data{std::forward<T>(data)}
+  {}
   virtual ~EManagerEvent() = default;
 
   Type getType() const;
-  ID getIdData() const;
+  Data getData() const;
 
 private:
   Type _t = Type::UNKNOWN;
-  ID _idData = 0;
+  Data _data;
 };
