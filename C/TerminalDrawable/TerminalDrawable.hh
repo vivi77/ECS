47458412ea6cc2b48cbb1162b055ad222e35281d
Victor Le <victor.le@epitech.eu>
2018-03-27 08:43:22 +0200

    TerminalDrawable: Terminal attributes added

--- C/TerminalDrawable/TerminalDrawable.hh
+++ C/TerminalDrawable/TerminalDrawable.hh
@@ -2,6 +2,7 @@
 
 #include "C/TerminalDrawable/terminaldrawable_export.h"
 #include "C/CRTPC.hpp"
+#include <bitset>
 
 struct TERMINALDRAWABLE_EXPORT TerminalDrawable : public CRTPC<TerminalDrawable>
 {
@@ -17,13 +18,28 @@ struct TERMINALDRAWABLE_EXPORT TerminalDrawable : public CRTPC<TerminalDrawable>
     WHITE,
   };
 
+  enum Attributes
+  {
+    NORMAL = 0,
+    BOLD = 1,
+    DIM = BOLD >> 1,
+    UNDERLINED = DIM >> 1,
+    BLINK = UNDERLINED >> 1,
+    REVERSE = BLINK >> 1,
+    HIDDEN = REVERSE >> 1,
+  };
+
+  using Attr = unsigned long long;
+
 public:
   TerminalDrawable(const char* = "");
   TerminalDrawable(const char*, const Color);
   TerminalDrawable(const char*, const Color, const Color);
+  TerminalDrawable(const char*, const Color, const Color, const Attr);
   virtual ~TerminalDrawable() = default;
 
   const char* sym = "";
   Color fgColor = Color::WHITE;
   Color bgColor = Color::WHITE;
+  std::bitset<6> attributes;
 };
