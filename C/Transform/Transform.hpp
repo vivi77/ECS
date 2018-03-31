7f20f155340e9b432cdcc10bb8174b6adfd42cbd
Victor Le <victor.le@epitech.eu>
2018-03-31 06:57:10 +0200

    [Not functional] Transform component changed

---
+++ C/Transform/Transform.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "C/CRTPC.hpp"
+#include "Utility/Vector.hpp"
+
+namespace lel::ecs::component
+{
+  template <typename T>
+  struct Transform : public CRTPC<Transform<T>>
+  {
+    using Position = Vector3<T>;
+    using Rotation = Vector3<T>;
+    using Scale = Vector3<T>;
+
+  public:
+    Transform(const T x, const T y, const T z)
+      : position{x, y, z}
+    {}
+    Transform(const Position& pos, const Rotation& rot, const Scale& sc)
+      : position{pos}
+      , rotation{rot}
+      , scale{sc}
+    {}
+    virtual ~Transform() = default;
+
+  public:
+    Position position;
+    Rotation rotation;
+    Scale scale;
+  };
+} /* !lel */
