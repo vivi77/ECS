d95df76640af2ac6d67a7d6521489cc9fc93b47a
Victor Le <victor.le@epitech.eu>
2018-04-03 10:52:37 +0200

    Started a Fwd.hh

---
+++ Utility/Fwd.hh
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace lel::ecs
+{
+  namespace system
+  {
+    using IDSystem = unsigned int;
+  } /* !system */
+
+  namespace component
+  {
+    using IDComponent = unsigned int;
+  } /* !component */
+
+  namespace event
+  {
+    using IDEvent = unsigned int;
+  } /* !event */
+} /* !lel::ecs */
