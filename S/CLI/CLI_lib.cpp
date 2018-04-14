3ab01d53c1c005548552dc841b84079ff3a2494a
Victor Le <victor.le@epitech.eu>
2018-04-14 02:47:55 +0200

    [Not working] Update CLI system according to the changement made on CRTPS

--- S/CLI/CLI_lib.cpp
+++ S/CLI/CLI_lib.cpp
@@ -3,9 +3,10 @@
 
 extern "C"
 {
-  CLISYSTEM_EXPORT lel::ecs::system::IS* create()
+  CLISYSTEM_EXPORT lel::ecs::system::IS* create(lel::ecs::CoreProxy* proxy)
   {
-    return new lel::ecs::system::CLI;
+    std::unique_ptr<lel::ecs::CoreProxy> proxyPtr{proxy};
+    return new lel::ecs::system::CLI(proxyPtr);
   }
 
   CLISYSTEM_EXPORT void destroy(lel::ecs::system::IS* ptr)
